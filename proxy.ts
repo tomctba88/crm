f07510178f6890282feb5e0c2259c7944dc39a48
Helinton <tomctba88@hotmail.com>
2026-05-03 15:21:59 -0300

    Libera arquivos publicos no proxy do CRM

--- proxy.ts
+++ proxy.ts
@@ -173,5 +173,7 @@ if (acessoDiretoAoCrm) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)',
+  ],
 }
